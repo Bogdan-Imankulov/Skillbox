ff0e8e0aefed8b8cb50c1afd7cc764fd03367017
YouAreMe1212 <baga.iman@yandex.ru>
2021-01-05 13:37:49 +0600

    homework_9.1

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,28 @@
+import net.sf.saxon.trans.SymbolicName;
+
+import java.io.File;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        final long[] size = {0};
+        File dir = new File(path);
+        try {
+            if (dir.isDirectory()){
+                Arrays.stream(dir.listFiles()).forEach(file -> {
+                    if (file.isFile())
+                        size[0] += file.length();
+                    else
+                        size[0] += calculateFolderSize(file.getAbsolutePath());
+                });
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return size[0];
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String pathToFolder = scanner.nextLine();
+        long size = FileUtils.calculateFolderSize(pathToFolder);
+        System.out.println(size);
     }
 }
