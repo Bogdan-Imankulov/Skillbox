2d53928e6c4b55cd016c22e21f7757db68a73848
YouAreMe1212 <revo.iman@yandex.ru>
2021-01-23 16:49:19 +0600

    homework_9.4.2

--- 09_FilesAndNetwork/homework_9.4/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.4/src/main/java/Main.java
@@ -7,26 +7,36 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
-    private static final String images = "D:\\Skillbox\\SonGit\\Skillbox\\09_FilesAndNetwork\\homework_9.4\\images\\";
+    private static final String images = "D:\\Skillbox\\SonGit\\Skillbox\\09_FilesAndNetwork\\" +
+            "homework_9.4\\images\\";
 
     public static void main(String[] args) {
         try {
             Document doc = Jsoup.connect(" https://lenta.ru").get();
             Elements links = doc.select("img");
-            final int[] i = {0};
+            final int[] imageNumber = {0};
             links.forEach(link -> {
                 try {
-                    String imageSrc = link.attr("src");
+                    String imageSrc = link.attr("abs:src");
+                    String[] srcSplit = imageSrc.split("/");
+                    String imageName = srcSplit[srcSplit.length-1];
                     URL url = new URL(imageSrc);
                     InputStream inpStream = url.openStream();
-                    File f = new File(images + i[0] + "image.jpg");
+                    File f = new File(images +
+                            /*imageSrc + ".jpg"*/
+                            /*imageNumber[0] + "image.jpg"*/
+                            imageName/* + ".jpg"*/);
+                    if (f.exists()) {
+                        f.delete();
+                    }
                     if (f.createNewFile()) {
-                        OutputStream outStream = new FileOutputStream(images + i[0] + "image.jpg");
+                        OutputStream outStream = new FileOutputStream(images +
+                                imageName
+                                /*imageSrc + ".jpg"*/
+                                /*imageName + ".jpg"*/
+                                /*imageNumber[0] + "image.jpg"*/);
                         byte[] bytes = new byte[2048];
                         int length;
                         while ((length = inpStream.read(bytes)) != -1) {
@@ -34,7 +44,7 @@ public class Main {
                         }
                         inpStream.close();
                         outStream.close();
-                        i[0]++;
+                        imageNumber[0]++;
                     }
 
                 } catch (Exception e) {
